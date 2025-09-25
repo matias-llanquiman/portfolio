6fc2c02bf68657dfbfe3fcf5d75efe2ade526598
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-25 17:41:34 -0300

    refactor: change functions for class and methods

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,35 +1,58 @@
-function animation() {
-  const canvas = document.getElementById("animation");
-  const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("animation");
+const ctx = canvas.getContext("2d");
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-  // Pintar todo el canvas
-  ctx.fillStyle = "rgb(29, 35, 42)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+// Pintar todo el canvas
+ctx.fillStyle = "rgb(29, 35, 42)";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+const particleRadius = 13;
 
-  const x1 = 800,
-    y1 = 100;
-  const x2 = 750,
-    y2 = 300;
-
-  for (let i = 0; i <= 12; i++) {
-    ctx.strokeStyle = "white";
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    x1++;
+class Particle {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
   }
 
-  ctx.fillStyle = "white";
-  const radius = 3;
-
-  ctx.beginPath();
-  ctx.arc(800, 300, radius, 0, Math.PI * 2);
-  ctx.arc(750, 300, radius, 0, Math.PI * 2);
-  ctx.fill();
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, particleRadius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
 }
 
-animation();
+let particles;
+let particlePositionX;
+let particlePositionY;
+
+let color;
+function init() {
+  particles = [];
+  let count = 2;
+  for (let i = 0; i < 60; i++) {
+    particlePositionX = Math.random() * canvas.width;
+    particlePositionY = Math.random() * canvas.height;
+    color = `rgb(${Math.floor(255 - 42.5 * count)} ${Math.floor(255 - 42.5 * count)} ${Math.floor(
+      255 - 42.5 * count,
+    )})`;
+    count++;
+    if (count > 4) {
+      count = 2;
+    }
+    particles.push(new Particle(particlePositionX, particlePositionY, color));
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  particles.forEach((particle) => {
+    particle.draw();
+  });
+}
+
+init();
+animate();
